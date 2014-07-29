b7cf12534f8844ba8fc635de36b1c8486846d61b
Tero Hasu <tero@ii.uib.no>
2014-07-29 13:37:44 +0200

    add launchers to info.rkt

--- info.rkt
+++ info.rkt
@@ -3,3 +3,7 @@
 (define name "Ractionary")
 (define compile-omit-paths '("retired"))
 (define deps '(("base" #:version "6.0")))
+(define racket-launcher-libraries
+  '("ractionary-make-dict.rkt" "ractionary-make-urls.rkt"))
+(define racket-launcher-names
+  '("ractionary-make-dict" "ractionary-make-urls"))
